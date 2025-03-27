4bcd08204ec611fed437cb5c291d9a6e2a32fbe7
Nora-haj <norahajduk@seattleacademy.org>
2025-03-27 10:03:36 -0700

    Herrow

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function showMessage() {
-    document.getElementById("message").innerText = "Hello, JavaScript is working!";
+    document.getElementById("message").innerText = "Hello,  welcome to cher's closet!";
     console.log("Button Clicked!");
 }
-
-document.getElementById("elementId").innerHTML = "This is a sprint projcet called cher's closet by Calvin Brost, and Nora Hajduk.\ The pourpose of this project is to hvae a way of archiving one's closet and being able to use this website to contruct an outfit for them from that archive,\ hence: cher's closet ";
