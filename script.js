709b9dd167524eb610a89a9937bab08eeacb0250
Calvin-Brost <calvinbrost@seattleacademy.org>
2025-03-27 10:40:58 -0700

    code
    wowowowowow

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 function showMessage() {
-    document.getElementById("message").innerText = "Hello,  welcome to cher's closet!";
-    console.log("Button Clicked!");
+    document.getElementById("message").innerText = "Hello, JavaScript is working!";
+    console.log("Button Clicked!")
 }
 
 
 const Clothingitemsvalues = [
-    {color : "Blue", item: "hat", type: "0", pattern: "false", image: "C:\\Users\\SAAS_User\\Downloads\\Blue Hat 1.avif"}
+    {color : "Blue", item: "hat", type: "0", pattern: "false", image: "C:\\Users\\SAAS_User\\Downloads\\Blue Hat 1.avif"},
+    {color : "white", item: "tshirt", type: "0", pattern: "false", image: "C:\Users\\SAAS_User\\Downloads\\white_T_1.jpg" },
     {color : "Black", item: "jacket", type: "bmbr", pattern: "false", image: "C:\\Users\\SAAS_User\\Downloads\\Bomber Jacket (black) 1.webp" }
 ]
+
+function showImage(accoryId) {
+    // const imageURLs = Clothingitemsvalues.map(clothes => clothes.image);
+    document.getElementById(accoryId).src = Clothingitemsvalues[1].image;
+    document.getElementById(accoryId).style.display = "block"
+}
+
+
+
+
+// const imageFiles = [file1, file2, file3]; // Assume these are File objects
+
+
