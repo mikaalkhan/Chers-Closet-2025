94248ff80c00d6c0e8d8ce7de2d9082997d7212e
Calvin-Brost <calvinbrost@seattleacademy.org>
2025-03-27 10:07:43 -0700

    code
    wow

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function showMessage() {
     document.getElementById("message").innerText = "Hello, JavaScript is working!";
     console.log("Button Clicked!");
 }
+
+
+const Clothingitemsvalues = [
+    {color : "Blue", item: "hat", type: "0", pattern: "false", image: "C:\\Users\\SAAS_User\\Downloads\\Blue Hat 1.avif"}
+    {color : "Black", item: "jacket", type: "bmbr", pattern: "false", image: "C:\\Users\\SAAS_User\\Downloads\\Bomber Jacket (black) 1.webp" }
+]
