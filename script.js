a303dd33d5ece69309299c68ba3cbaec7545ad8c
Calvin-Brost <calvinbrost@seattleacademy.org>
2025-03-31 12:32:25 -0700

    wiow
    woah
    
    Co-Authored-By: Nora-haj <203163555+Nora-haj@users.noreply.github.com>

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const Clothingitemsvalues = [
     {color : "Blue", item: "hat", type: "0", pattern: "false", image: "C:\\Users\\SAAS_User\\Downloads\\Blue Hat 1.avif"},
     {color : "white", item: "tshirt", type: "0", pattern: "false", image: "C:\Users\\SAAS_User\\Downloads\\white_T_1.jpg" },
     {color : "Black", item: "jacket", type: "bmbr", pattern: "false", image: "C:\\Users\\SAAS_User\\Downloads\\Bomber Jacket (black) 1.webp" }
+    {color : "Paleblue", item: "pants", type: "jeans", pattern: "false", image: "C:\Users\SAAS_User\Downloads\Light Jeans 1.avif" }
+    {color : "Blue White", item: "shoes", type: "sambas", pattern: "false", image: "C:\Users\SAAS_User\Downloads\shoes samba white and blue.avif" }
 ]
 
 function showImage(accoryId) {
@@ -16,8 +18,12 @@ function showImage(accoryId) {
         document.getElementById(accoryId).src = Clothingitemsvalues[0].image;
     } else if (accoryId == "Shirtid") {
         document.getElementById(accoryId).src = Clothingitemsvalues[1].image;
-    } else if (accoryId == "Pantid") {
+    } else if (accoryId == "Jacketid") {
         document.getElementById(accoryId).src = Clothingitemsvalues[2].image;
+    } else if (accoryId == "Shoeid") {
+        document.getElementById(accoryId).src = Clothingitemsvalues[3].image;
+    } else if (accoryId == "Pantid") {
+        document.getElementById(accoryId).src = Clothingitemsvalues[4].image;
     }
 
     document.getElementById(accoryId).style.display = "block"
