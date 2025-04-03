4f5ac74a2c1961a7dbcc5d9e0f31af7838bc0dc9
Calvin-Brost <calvinbrost@seattleacademy.org>
2025-04-03 11:48:54 -0700

    raaaaaaaaaaaaaaaasdfghjkluygvbbvb
    hc,gnb,,v,vhj
    
    Co-Authored-By: Nora-haj <203163555+Nora-haj@users.noreply.github.com>

--- script.js
+++ script.js
@@ -3,28 +3,42 @@ function showMessage() {
     console.log("Button Clicked!")
 }
 
+const hatvalues = [
+    {color : "Blue", type: "0", pattern: "false", image: "C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\Blue Hat 1.avif"}
+]
 
-const Clothingitemsvalues = [
-    {color : "Blue", item: "hat", type: "0", pattern: "false", image: "C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\Blue Hat 1.avif"},
-    {color : "white", item: "tshirt", type: "0", pattern: "false", image: "C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\white_T_1.jpg" },
-    {color : "Black", item: "jacket", type: "bmbr", pattern: "false", image:"C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\Bomber Jacket (black) 1.webp" },
-    {color : "Paleblue", item: "pants", type: "jeans", pattern: "false", image: "C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\Light Jeans 1.avif" },
-    {color : "Blue White", item: "shoes", type: "sambas", pattern: "false", image: "C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\shoes samba white and blue.avif" }
+const shirtvalues = [
+    {color : "white", type: "0", pattern: "false", image: "C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\white_T_1.jpg" },
+    {color : "ivory", type: "0", pattern: "false", image: "C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\Ivory-tshirt.webp"}
+]
+
+const jacketvalues = [
+    {color : "Black", type: "bmbr", pattern: "false", image:"C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\Bomber Jacket (black) 1.webp" },
+    {color : "Navy", type: "swtr", pattern: "false", image: "C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\sweatter-navy-1.avif" }
+]
+
+const pantvalues = [
+    {color : "Paleblue", type: "jeans", pattern: "false", image: "C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\Light Jeans 1.avif" },
+    {color : "ivory", type: "ribbed", pattern: "false", image: "C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\Ivory-pants-ribbed.webp" }
+]
+
+const shoesvalues = [
+    {color : "Blue White", type: "sambas", pattern: "false", image: "C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\shoes samba white and blue.avif" },
+    {color : "Blue White", type: "sambas", pattern: "false", image: "C:\\Users\\SAAS_User\\Documents\\GitHub\\Chers-Closet-2025\\images\\shoes-white&blue-2.webp"}
 ]
 
 function showImage(accoryId) {
     // const imageURLs = Clothingitemsvalues.map(clothes => clothes.image);
     if (accoryId == "Hatid") {
-        document.getElementById(accoryId).src = Clothingitemsvalues[0].image;
-        document.getElementById("OId").style.display = "none"
+        document.getElementById(accoryId).src = hatvalues[0].image;
     } else if (accoryId == "Shirtid") {
-        document.getElementById(accoryId).src = Clothingitemsvalues[1].image;
+        document.getElementById(accoryId).src = shirtvalues[0].image;
     } else if (accoryId == "Jacketid") {
-        document.getElementById(accoryId).src = Clothingitemsvalues[2].image;
+        document.getElementById(accoryId).src = jacketvalues[0].image;
     } else if (accoryId == "Shoeid") {
-        document.getElementById(accoryId).src = Clothingitemsvalues[4].image;
+        document.getElementById(accoryId).src = pantvalues[0].image;
     } else if (accoryId == "Pantid") {
-        document.getElementById(accoryId).src = Clothingitemsvalues[3].image;
+        document.getElementById(accoryId).src = shoesvalues[0].image;
     }
 
     document.getElementById(accoryId).style.display = "block"
